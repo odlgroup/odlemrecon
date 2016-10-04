2a09a4f862cf4d0001dfca0057f1b3815f8ea9af
Jonas Adler <jonasadl@kth.se>
2016-10-04 14:27:41 +0200

    Add copyright tags to examples

--- examples/conjugate_gradient.py
+++ examples/conjugate_gradient.py
@@ -1,3 +1,22 @@
+# Copyright 2014-2016 The ODL development group
+#
+# This file is part of ODL.
+#
+# ODL is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ODL is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ODL.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Example of PET tomography problem using the GGLS method."""
+
 import odl
 import odlemrecon
 
@@ -17,10 +36,10 @@ ran = odl.uniform_discr([0]*3, ran_shape, ran_shape)
 op = odlemrecon.EMReconForwardProjector(settings_file_name, space, ran)
 
 phantom = odl.phantom.shepp_logan(space, modified=True)
-phantom.show()
+phantom.show('phantom')
 
 projection = op(phantom)
-projection.show()
+projection.show('projection')
 
 callback = odl.solvers.CallbackShow()
 
